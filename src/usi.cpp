944fa05e577d77e952cafce1510dcd30637339c7
HiraokaTakuya <hiraoka64@gmail.com>
2015-11-28 20:10:38 +0900

    名前を開発版に戻す。

--- src/usi.cpp
+++ src/usi.cpp
@@ -424,7 +424,7 @@ void measureGenerateMoves(const Position& pos) {
 #endif
 
 #ifdef NDEBUG
-const std::string MyName = "Apery_Twig_SDT3";
+const std::string MyName = "Apery";
 #else
 const std::string MyName = "Apery Debug Build";
 #endif
