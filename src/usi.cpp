e923d2764e54795bab465ee7ce87b0de2529acf1
HiraokaTakuya <hiraoka64@gmail.com>
2016-01-12 23:14:54 +0900

    key取得コマンド追加

--- src/usi.cpp
+++ src/usi.cpp
@@ -459,6 +459,7 @@ void Searcher::doUSICommandLoop(int argc, char* argv[]) {
 #if !defined MINIMUL
 		// 以下、デバッグ用
 		else if (token == "bench"    ) benchmark(pos);
+		else if (token == "key"      ) SYNCCOUT << pos.getKey() << SYNCENDL;
 		else if (token == "d"        ) pos.print();
 		else if (token == "s"        ) measureGenerateMoves(pos);
 		else if (token == "t"        ) std::cout << pos.mateMoveIn1Ply().toCSA() << std::endl;
