c8a040c174bdacd7a112b6accfa5abb91dc6ab2a
HiraokaTakuya <hiraoka64@gmail.com>
2016-05-05 23:49:58 +0900

    名前を Apery_WCSC26 に変更

--- src/usi.cpp
+++ src/usi.cpp
@@ -392,7 +392,7 @@ void measureGenerateMoves(const Position& pos) {
 #endif
 
 #ifdef NDEBUG
-const std::string MyName = "Apery";
+const std::string MyName = "Apery_WCSC26";
 #else
 const std::string MyName = "Apery Debug Build";
 #endif
