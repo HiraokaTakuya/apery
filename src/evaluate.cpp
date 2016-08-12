f25b27baeabf8cb4abdf224ef9dba3b9fc17a2fd
HiraokaTakuya <hiraoka64@gmail.com>
2016-08-13 03:34:55 +0900

    評価関数バイナリのpathを変更

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -5,7 +5,7 @@
 
 KPPBoardIndexStartToPiece g_kppBoardIndexStartToPiece;
 
-std::string Evaluater::evalDir = "20160810";
+std::string Evaluater::evalDir = "20160812";
 EvaluateHashTable g_evalTable;
 
 const int kppArray[31] = {
