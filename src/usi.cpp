cf9a616ca59392f5225532d46928ec791a78975a
HiraokaTakuya <hiraoka64@gmail.com>
2017-11-12 13:45:25 +0900

    第5回将棋電王トーナメントバージョン

--- src/usi.cpp
+++ src/usi.cpp
@@ -123,7 +123,7 @@ void OptionsMap::init(Searcher* s) {
     (*this)["Minimum_Thinking_Time"]       = USIOption(20, 0, INT_MAX);
     (*this)["Threads"]                     = USIOption(cpuCoreCount(), 1, MaxThreads, onThreads, s);
 #ifdef NDEBUG
-    (*this)["Engine_Name"]                 = USIOption("Apery");
+    (*this)["Engine_Name"]                 = USIOption("Apery_SDT5");
 #else
     (*this)["Engine_Name"]                 = USIOption("Apery Debug Build");
 #endif
