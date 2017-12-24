78f6de39ec57cf063140b2f72e752aafea958d94
qhapaq_49 <qh@example.com>
2017-12-24 23:48:05 +0900

    conv_teacherを追加

--- obj/common.d
+++
@@ -1,5 +0,0 @@
-../obj/common.o: common.cpp common.hpp ifdef.hpp
-
-common.hpp:
-
-ifdef.hpp:
--- obj/mt64bit.d
+++
@@ -1,7 +0,0 @@
-../obj/mt64bit.o: mt64bit.cpp mt64bit.hpp common.hpp ifdef.hpp
-
-mt64bit.hpp:
-
-common.hpp:
-
-ifdef.hpp:
